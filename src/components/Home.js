38d54edbd534fe4077bdef97a0c90c9decb97677
NeeleshRoy <neeleshroy.2013@gmail.com>
2019-09-25 02:18:29 +0530

    added cta

--- src/components/Home.js
+++ src/components/Home.js
@@ -88,6 +88,20 @@ class Home extends Component {
               </Link>
             </div>
           </div>
+
+          <div className='feature-portfolio'>
+            <img
+              src='/images/projects/cta_6.jpg'
+              alt='Most recent independently designed brochures to be used during the client visit as an agenda..'
+            />
+            <div className='feature-portfolio-info'>
+              <h2>Client Visit Agendas</h2>
+              <p>Most recent independently designed brochures to be used during the client visit as an agenda.</p>
+              <Link className='button-primary' to='/projects/brochures'>
+                See the designs &#8594;
+              </Link>
+            </div>
+          </div>
           <div className='thanks'>
             <p>Copyright &copy; 2019 by Neelesh Roy</p>
             <p>All Rights Reserved.</p>
